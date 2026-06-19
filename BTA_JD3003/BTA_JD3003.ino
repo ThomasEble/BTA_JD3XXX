2c0a97f16c0ebb995f4e1b3ff034d8c17326ef93
Thomas Scooter Eble <thomasjeble@gmail.com>
2026-06-19 14:39:34 -0400

    not sure if this library needs to stay here, might get deleted soon. otherwise, BTA_JD3003 code

--- BTA_JD3003/BTA_JD3003.ino
+++ BTA_JD3003/BTA_JD3003.ino
@@ -1,24 +1,25 @@
 #include "AudioTools.h"
 #include "BluetoothA2DPSink.h"
 
-AnalogAudioStream out;
+I2SStream out;
 BluetoothA2DPSink a2dp_sink(out);
 
 void setup() {
   Serial.begin(115200);
 
-  //Added this bit to output MONO to both GPIO25 and 26
   auto cfg = out.defaultConfig(TX_MODE);
   cfg.sample_rate = 44100;
   cfg.channels = 2;
   cfg.bits_per_sample = 16;
+  cfg.pin_bck  = 26;
+  cfg.pin_ws   = 25;
+  cfg.pin_data = 22;
+  // bump DMA buffering for more slack against underruns:
+  cfg.buffer_count = 8;
+  cfg.buffer_size  = 512;
   out.begin(cfg);
-  a2dp_sink.set_mono_downmix(true);
 
-  a2dp_sink.start("InternalDAC");  
-
+  a2dp_sink.start("BTA_JD3003");
 }
 
-
-void loop() {
-}
+void loop() {}
